9220cb8e3a6d1830d8221da0fe5f95a2d9d338ff
jrotolo <jrotol5@lsu.edu>
2015-03-14 13:42:27 -0500

    Stubbed TransDec and TransTy methods

--- prog3/src/Semant/TransDec.java
+++ prog3/src/Semant/TransDec.java
@@ -7,4 +7,27 @@ public class TransDec extends Trans {
 	public TransDec(Env e) {
 		env = e;
 	}
+
+	public void transDec(Absyn.Dec d) {
+		if (d instanceof Absyn.FunctionDec)
+			transDec((Absyn.FunctionDec)d);
+		else if (d instanceof Absyn.VarDec)
+			transDec((Absyn.VarDec)d);
+		else if (d instanceof Absyn.TypeDec)
+			transDec((Absyn.TypeDec)d);
+		else
+			throw new Error("TransDec.transDec");
+	}
+
+	public void transDec(Absyn.FunctionDec d) {
+
+	}
+
+	public void transDec(Absyn.VarDec d) {
+
+	}
+
+	public void transDec(Absyn.TypeDec d) {
+
+	}
 }
--- prog3/src/Semant/TransTy.java
+++ prog3/src/Semant/TransTy.java
@@ -7,4 +7,29 @@ public class TransTy extends Trans {
 	public TransTy(Env e) {
 		env = e;
 	}
+
+	public Type transTy(Absyn.Ty t) {
+		if (t instanceof Absyn.ArrayTy)
+			return transTy((Absyn.ArrayTy)t);
+		else if (t instanceof Absyn.NameTy)
+			return transTy((Absyn.NameTy)t);
+		else if (t instanceof Absyn.RecordTy)
+			return transTy((Absyn.RecordTy)t);
+		else throw new Error("TransTy.transTy");
+	}
+
+	public Type transTy(Absyn.ArrayTy t) {
+		Types.NAME type = (Types.NAME)env.tenv.get(t.typ);
+		return new Types.ARRAY(type);
+	}
+
+	public Type transTy(Absyn.NameTy t) {
+		Types.NAME type = (Types.NAME)env.tenv.get(t.name);
+		return type;
+	}
+
+	public Type transTy(Absyn.RecordTy t) {
+		return new Types.NIL();
+	}
+		
 }
