0e8efb3814eda68987fceadbc19d2af9352aba22
jrotolo <jrotol5@lsu.edu>
2015-03-28 23:23:05 -0500

    Updated VarEntry

--- prog4/src/Semant/VarEntry.java
+++ prog4/src/Semant/VarEntry.java
@@ -3,7 +3,9 @@ package Semant;
 public class VarEntry extends Entry {
   Translate.Access access;
   public Types.Type ty;
-  VarEntry(Types.Type t) {
+  
+  VarEntry(Types.Type t, Translate.Access a) {
     ty = t;
+    access = a;
   }
 }
